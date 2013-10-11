1d49a6c6c89295b53846ac54ab8b148a3910e348
Andrew Gridnev <andrew.gridnev@gmail.com>
2013-10-11 21:07:46 +0700

    bump version

--- lib/missing_validators/version.rb
+++ lib/missing_validators/version.rb
@@ -1,5 +1,5 @@
 # Provides a collection of custom validators that are often required in Rails applications.
 module MissingValidators
   # Gem version.
-  VERSION = "0.4.2"
+  VERSION = "0.5.1"
 end
