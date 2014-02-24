a3761440058dbc80214f9fd65a6f503d5db92f15
Andrew Gridnev <andrew.gridnev@digitaldialogue.com.au>
2014-02-25 10:47:58 +1100

    Remove compartibility for 1.9.2

--- lib/missing_validators/version.rb
+++ lib/missing_validators/version.rb
@@ -1,5 +1,5 @@
 # Provides a collection of custom validators that are often required in Rails applications.
 module MissingValidators
   # Gem version.
-  VERSION = "0.8.0"
+  VERSION = "0.8.1"
 end
