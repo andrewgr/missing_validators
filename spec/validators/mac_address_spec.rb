d3f9ff478f606ae035a90d73af9b101e0ada2c34
Nick Peelman <nick@peelman.us>
2014-03-21 12:25:31 -0400

    Adding some more specs and more formats:
    08.00.2b.01.02.03
    08 00 2b 01 02 03
    08002b.010203
    0800-2b01-0203
    0800 2b01 0203

--- spec/validators/mac_address_spec.rb
+++ spec/validators/mac_address_spec.rb
@@ -15,18 +15,36 @@ describe MacAddressValidator do
     it { should ensure_valid_mac_address_format_of(:mac) }
     it { should_not ensure_valid_mac_address_format_of(:name) }
 
+    # Valid formats
     it { should allow_value("08:00:2b:01:02:03").for(:mac) }
     it { should allow_value("08-00-2b-01-02-03").for(:mac) }
+    it { should allow_value("08.00.2b.01.02.03").for(:mac) }
+    it { should allow_value("08 00 2b 01 02 03").for(:mac) }
     it { should allow_value("08002b:010203").for(:mac) }
+    it { should allow_value("08002b.010203").for(:mac) }
     it { should allow_value("08002b-010203").for(:mac) }
     it { should allow_value("0800.2b01.0203").for(:mac) }
+    it { should allow_value("0800-2b01-0203").for(:mac) }
+    it { should allow_value("0800 2b01 0203").for(:mac) }
     it { should allow_value("08002b010203").for(:mac) }
 
+    # Mixed Separators
+    it { should_not allow_value("08-00:2b:01:02:03").for(:mac) }
+    it { should_not allow_value("08.00:2b:01:02:03").for(:mac) }
+    it { should_not allow_value("08 00:2b:01:02:03").for(:mac) }
+    it { should_not allow_value("0800-2b01:0203").for(:mac) }
+    it { should_not allow_value("0800 2b01 0203").for(:mac) }
+
+    # Too Short
     it { should_not allow_value("08:00:2b:01:02").for(:mac) }
+    it { should_not allow_value("08-00-2b-01-02").for(:mac) }
+
+    # Too Long
     it { should_not allow_value("08:00:2b:01:02:03:04").for(:mac) }
+
+    # Non-Hex Characters
     it { should_not allow_value("qq:00:00:00:00:00").for(:mac) }
 
-    it { should_not allow_value("08-00-2b-01-02").for(:mac) }
 
     it { should_not allow_value("invalid").for(:mac) }
   end
