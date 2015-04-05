fa12e7b215865b74a40c88709a5e861587ef7b50
Andrei Gridnev <andrew.gridnev@gmail.com>
2015-04-06 00:35:41 +1000

    Make Code Climate happier

--- lib/missing_validators/validators/equality_validator.rb
+++ lib/missing_validators/validators/equality_validator.rb
@@ -9,12 +9,22 @@ class EqualityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     to = options[:to]
 
-    equal_to_value = to.respond_to?(:call) ? options[:to].call(record) : to
+    reference_value = to.respond_to?(:call) ? options[:to].call(record) : to
 
-    return if value == equal_to_value
+    return if compare_value(value, reference_value)
 
     record.errors[attribute] << options.fetch(:message) do
-      I18n.t('errors.messages.equality', value: equal_to_value)
+      I18n.t(translation_key, value: reference_value)
     end
+  end
+
+  private
+
+  def compare_value(value, reference_value)
+    value == reference_value
+  end
+
+  def translation_key
+    'errors.messages.equality'
   end
 end
--- lib/missing_validators/validators/inequality_validator.rb
+++ lib/missing_validators/validators/inequality_validator.rb
@@ -1,20 +1,13 @@
 # Checks if the value of an attribute is equal to a value or proc result.
 #
-class InequalityValidator < ActiveModel::EachValidator
-  # Checks if an attribute value is unequal to another attrubute value.
-  #
-  # @param [Object] record object to validate
-  # @param [String] attribute name of the object attribute to validate
-  # @param [Object] value attribute value
-  def validate_each(record, attribute, value)
-    to = options[:to]
+class InequalityValidator < EqualityValidator
+  private
 
-    unequal_to_value = to.respond_to?(:call) ? options[:to].call(record) : to
+  def compare_value(value, reference_value)
+    value != reference_value
+  end
 
-    return if value != unequal_to_value
-
-    record.errors[attribute] << options.fetch(:message) do
-      I18n.t('errors.messages.inequality', value: unequal_to_value)
-    end
+  def translation_key
+    'errors.messages.inequality'
   end
 end
