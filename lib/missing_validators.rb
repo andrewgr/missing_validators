73a21f6849386ff3436f224016356e82ff8415bb
Andrew Tomaka <atomaka@gmail.com>
2016-07-05 16:05:01 -0400

    Latitude and longitude Rspec matchers

--- lib/missing_validators.rb
+++ lib/missing_validators.rb
@@ -21,6 +21,8 @@ if defined?(RSpec)
   require 'missing_validators/matchers/ensure_valid_url_format_of'
   require 'missing_validators/matchers/ensure_valid_mac_address_format_of'
   require 'missing_validators/matchers/ensure_valid_imei_format_of'
+  require 'missing_validators/matchers/ensure_valid_latitude_format_of'
+  require 'missing_validators/matchers/ensure_valid_longitude_format_of'
 end
 
 I18n.load_path << File.expand_path('../locales/en.yml', __FILE__)
